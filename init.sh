d958139ace7d9119920e2f9e6ad42c99f8e044d0
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-01-10 22:17:08 +0100

    fix: record dependencies

--- init.sh
+++ init.sh
@@ -2,11 +2,12 @@
 
 Rscript -e 'renv::init(bare = FALSE)'
 Rscript -e 'renv::install("rmarkdown")'
+Rscript -e 'renv::snapshot(type = "all")'
 
 python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install jupyter
-python3 freeze > requirements.txt
+python3 -m pip freeze > requirements.txt
 
 julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
 julia --project=. -e 'using Pkg; Pkg.add("IJulia")'
