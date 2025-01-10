2fdf450bec4897f638983e9d1352d7a2c65c7693
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-01-10 22:12:25 +0100

    fix: freeze python dependencies

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ Rscript -e 'renv::install("rmarkdown")'
 python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install jupyter
+python3 freeze > requirements.txt
 
 julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
 julia --project=. -e 'using Pkg; Pkg.add("IJulia")'
