52b58c08ba80883b54d7aaf5df7c1652a71b8d24
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-01-11 17:42:23 +0100

    fix: use juliaup path

--- .github/.devcontainer/quarto-computing-dependencies/install.sh
+++ .github/.devcontainer/quarto-computing-dependencies/install.sh
@@ -3,8 +3,34 @@
 set -e
 
 export DEBIAN_FRONTEND=noninteractive
-USERNAME=${_REMOTE_USER:-"vscode"}
+
 DEPENDENCIES=${DEPENDENCIES:-"all"}
+
+USERNAME=${USERNAME:-${_REMOTE_USER:-"automatic"}}
+
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+  exit 1
+fi
+
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+  USERNAME=""
+  POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+  for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+    if id -u "${CURRENT_USER}" >/dev/null 2>&1; then
+      USERNAME=${CURRENT_USER}
+      break
+    fi
+  done
+  if [ "${USERNAME}" = "" ]; then
+    USERNAME=root
+  fi
+elif [ "${USERNAME}" = "none" ] || ! id -u "${USERNAME}" >/dev/null 2>&1; then
+  USERNAME=root
+fi
 
 quarto_r_deps() {
   su "${USERNAME}" -c "Rscript -e 'pak::pkg_install(\"rmarkdown\")'"
@@ -15,7 +41,7 @@ quarto_python_deps() {
 }
 
 quarto_julia_deps() {
-  su "${USERNAME}" -c "julia -e 'using Pkg; Pkg.add("IJulia")'"
+  su "${USERNAME}" -c "~/.juliaup/bin/julia -e 'using Pkg; Pkg.add(\"IJulia\")'"
 }
 
 case ${DEPENDENCIES} in
