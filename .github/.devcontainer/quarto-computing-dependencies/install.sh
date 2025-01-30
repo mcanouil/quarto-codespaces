1fc5163caf9d5dae60f45e97470930ef212988bf
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-01-30 15:07:03 +0000

    feat: use options to set dependencies

--- .github/.devcontainer/quarto-computing-dependencies/install.sh
+++ .github/.devcontainer/quarto-computing-dependencies/install.sh
@@ -6,6 +6,10 @@ export DEBIAN_FRONTEND=noninteractive
 
 USERNAME=${USERNAME:-${_REMOTE_USER:-"automatic"}}
 
+R_DEPS=${R_DEPS:-"rmarkdown"}
+PYTHON_DEPS=${PYTHON_DEPS:-"jupyter,papermill"}
+JULIA_DEPS=${JULIA_DEPS:-"IJulia"}
+
 set -e
 
 if [ "$(id -u)" -ne 0 ]; then
@@ -31,17 +35,23 @@ elif [ "${USERNAME}" = "none" ] || ! id -u "${USERNAME}" >/dev/null 2>&1; then
 fi
 
 quarto_r_deps() {
-  su "${USERNAME}" -c "Rscript -e 'pak::pkg_install(\"rmarkdown\")'"
+  local deps=$1
+  deps=$(echo "${deps}" | sed 's/,/","/g')
+  su "${USERNAME}" -c "Rscript -e 'pak::pkg_install(c(\"${deps}\"))'"
 }
 
 quarto_python_deps() {
-  python3 -m pip install jupyter papermill
+  local deps=$1
+  deps=$(echo "${deps}" | sed 's/,/ /g')
+  python3 -m pip install ${deps}
 }
 
 quarto_julia_deps() {
-  su "${USERNAME}" -c "~/.juliaup/bin/julia -e 'using Pkg; Pkg.add(\"IJulia\")'"
+  local deps=$1
+  deps=$(echo "${deps}" | sed 's/,/","/g')
+  su "${USERNAME}" -c "~/.juliaup/bin/julia -e 'using Pkg; Pkg.add.([\"${deps}\"])'"
 }
 
-quarto_r_deps
-quarto_python_deps
-quarto_julia_deps
+quarto_r_deps ${R_DEPS}
+quarto_python_deps ${PYTHON_DEPS}
+quarto_julia_deps ${JULIA_DEPS}
