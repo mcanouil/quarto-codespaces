9cb029bdd781c8de1683b06fc8faffee9744d540
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-03-09 09:33:26 +0100

    feat: add support for uv Python environment management (#20)

--- .github/.devcontainer/quarto-computing-dependencies/install.sh
+++ .github/.devcontainer/quarto-computing-dependencies/install.sh
@@ -10,8 +10,6 @@ R_DEPS=${RDEPS:-"rmarkdown"}
 PYTHON_DEPS=${PYTHONDEPS:-"jupyter,papermill"}
 JULIA_DEPS=${JULIADEPS:-"IJulia"}
 
-set -e
-
 if [ "$(id -u)" -ne 0 ]; then
   echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
   exit 1
