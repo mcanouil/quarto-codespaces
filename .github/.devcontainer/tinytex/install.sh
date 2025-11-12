09baf0dc21951554fefb4f8e7bb34b489d4639db
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-11-12 20:23:28 +0100

    fix: add environment variables for TinyTeX configuration (#77)

--- .github/.devcontainer/tinytex/install.sh
+++ .github/.devcontainer/tinytex/install.sh
@@ -70,13 +70,20 @@ install_tinytex() {
   chgrp -R tinytex "${TINYTEX_DIR}/.TinyTeX"
   chmod -R 775 "${TINYTEX_DIR}/.TinyTeX"
   
-  ln -s "${TINYTEX_DIR}/.TinyTeX" "${HOME}/.TinyTeX"
-  ln -s "${TINYTEX_DIR}/.TinyTeX" "/home/${USERNAME}/.TinyTeX"
-  ln -s "${TINYTEX_DIR}/.TinyTeX/bin/$(uname -m)-linux" /usr/local/bin/tinytex
+  # ln -s "${TINYTEX_DIR}/.TinyTeX" "${HOME}/.TinyTeX"
+  # ln -s "${TINYTEX_DIR}/.TinyTeX" "/home/${USERNAME}/.TinyTeX"
+  # ln -s "${TINYTEX_DIR}/.TinyTeX/bin/$(uname -m)-linux" /usr/local/bin/tinytex
     
   # Add TinyTeX binaries to PATH for all users via /etc/profile.d/
   echo "export PATH=\"${TINYTEX_DIR}/.TinyTeX/bin/$(uname -m)-linux:\${PATH}\"" > /etc/profile.d/tinytex.sh
   chmod 644 /etc/profile.d/tinytex.sh
+
+  # Set TEXMFVAR and TEXMFCONFIG for all users
+  (
+    echo "export TEXMFVAR=\"\${HOME}/.texlive/texmf-var\""
+    echo "export TEXMFCONFIG=\"\${HOME}/.texlive/texmf-config\""
+  ) > /etc/profile.d/tinytex_env.sh
+  chmod 644 /etc/profile.d/tinytex_env.sh
 }
 
 install_tinytex
